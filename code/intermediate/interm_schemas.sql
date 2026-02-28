ec80324187bab219e5c87cbc083dd68fbaa9c852
juan <sxhxgun@gmail.com>
2026-02-28 11:38:12 -0800

    Modify the intermediate schema by deleting redundant tables, and only using one to obtain the tconst value for movies that have been rated at least once

--- code/intermediate/interm_schemas.sql
+++ code/intermediate/interm_schemas.sql
@@ -1,30 +1,14 @@
 CREATE SCHEMA IF NOT EXISTS intermediate;
 
-CREATE TABLE IF NOT EXISTS intermediate.movie_tconsts AS (
-    SELECT "tconst" AS tconst
-    FROM staging.title_basics
-    WHERE "titleType" = 'movie'
-);
+-- Delete tables that are no longer needed
+DROP TABLE IF EXISTS intermediate.movie_tconsts;
+DROP TABLE IF EXISTS intermediate.movie_ratings;
+DROP TABLE IF EXISTS intermediate.movie_ratings_stats;
 
-CREATE TABLE IF NOT EXISTS intermediate.movie_ratings AS (
-    SELECT "tconst" AS tconst,
-    CAST("averageRating" AS double precision) AS averagerating,
-    CAST("numVotes" AS integer) AS numvotes
-    FROM staging.title_ratings
-    WHERE "tconst" IN (SELECT tconst FROM intermediate.movie_tconsts)
-);
-
-CREATE TABLE IF NOT EXISTS intermediate.movie_ratings_stats AS (
-    SELECT
-    MIN(numvotes) AS minvotes,
-    MAX(numvotes) AS maxvotes,
-    AVG(numvotes) AS avgvotes,
-    mode() WITHIN GROUP (ORDER BY numvotes ASC) AS votesmode,
-    percentile_cont(0.25) WITHIN GROUP (ORDER BY numvotes ASC)
-        AS p25votes,
-    percentile_cont(0.50) WITHIN GROUP (ORDER BY numvotes ASC)
-    AS p50votes,
-    percentile_cont(0.75) WITHIN GROUP (ORDER BY numvotes ASC)
-        AS p75votes
-    FROM intermediate.movie_ratings
+-- Contains the tconst values for movies that have been rated at least once
+CREATE TABLE IF NOT EXISTS intermediate.rated_movie_tconsts AS (
+    SELECT tb.tconst AS tconst
+    FROM staging.title_basics tb INNER JOIN staging.title_ratings tr
+    ON tb.tconst = tr.tconst
+    WHERE tb."titleType" = 'movie'
 );
