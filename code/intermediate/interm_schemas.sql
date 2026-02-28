818216e5296806e5e06c50c5eacfd11b04d8bc41
juan <sxhxgun@gmail.com>
2026-02-28 11:22:50 -0800

    Modify the names of the intermediate tables to use more accurate names.

--- code/intermediate/interm_schemas.sql
+++ code/intermediate/interm_schemas.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS intermediate.movie_tconsts AS (
     WHERE "titleType" = 'movie'
 );
 
-CREATE TABLE IF NOT EXISTS intermediate.title_ratings AS (
+CREATE TABLE IF NOT EXISTS intermediate.movie_ratings AS (
     SELECT "tconst" AS tconst,
     CAST("averageRating" AS double precision) AS averagerating,
     CAST("numVotes" AS integer) AS numvotes
@@ -14,17 +14,17 @@ CREATE TABLE IF NOT EXISTS intermediate.title_ratings AS (
     WHERE "tconst" IN (SELECT tconst FROM intermediate.movie_tconsts)
 );
 
-DROP TABLE IF EXISTS intermediate.ratings_stats;
-CREATE TABLE IF NOT EXISTS intermediate.ratings_stats AS (
+CREATE TABLE IF NOT EXISTS intermediate.movie_ratings_stats AS (
     SELECT
     MIN(numvotes) AS minvotes,
     MAX(numvotes) AS maxvotes,
     AVG(numvotes) AS avgvotes,
+    mode() WITHIN GROUP (ORDER BY numvotes ASC) AS votesmode,
     percentile_cont(0.25) WITHIN GROUP (ORDER BY numvotes ASC)
         AS p25votes,
-    percentile_cont(0.55) WITHIN GROUP (ORDER BY numvotes ASC)
+    percentile_cont(0.50) WITHIN GROUP (ORDER BY numvotes ASC)
     AS p50votes,
     percentile_cont(0.75) WITHIN GROUP (ORDER BY numvotes ASC)
         AS p75votes
-    FROM intermediate.title_ratings
+    FROM intermediate.movie_ratings
 );
