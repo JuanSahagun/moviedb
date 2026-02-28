8683d9837f24431e9a91e18e8e2a1036320079a7
juan <sxhxgun@gmail.com>
2026-02-28 12:46:48 -0800

    Create an intermediate table of title_basics: Only retain rated-movies, remove unecessary attributes, and cast to correct datatypes.

--- code/intermediate/interm_schemas.sql
+++ code/intermediate/interm_schemas.sql
@@ -1,14 +1,23 @@
 CREATE SCHEMA IF NOT EXISTS intermediate;
 
--- Delete tables that are no longer needed
-DROP TABLE IF EXISTS intermediate.movie_tconsts;
-DROP TABLE IF EXISTS intermediate.movie_ratings;
-DROP TABLE IF EXISTS intermediate.movie_ratings_stats;
-
 -- Contains the tconst values for movies that have been rated at least once
 CREATE TABLE IF NOT EXISTS intermediate.rated_movie_tconsts AS (
     SELECT tb.tconst AS tconst
     FROM staging.title_basics tb INNER JOIN staging.title_ratings tr
     ON tb.tconst = tr.tconst
     WHERE tb."titleType" = 'movie'
+);
+
+-- Only retain attributes relevant to movies, and CAST to correct data types.
+CREATE TABLE IF NOT EXISTS intermediate.movie_basics AS (
+    SELECT
+        "tconst" AS tconst,
+        "primaryTitle" AS primarytitle,
+        "originalTitle" AS originaltitle,
+        CAST("isAdult" AS boolean) AS isadult,
+        CAST("startYear" AS integer) AS startyear,
+        CAST("runtimeMinutes" AS integer) AS runtimeminutes,
+        CAST("genres" AS VARCHAR(32)) AS genres
+    FROM staging.title_basics
+    WHERE "tconst" IN (SELECT tconst FROM intermediate.rated_movie_tconsts)
 );
