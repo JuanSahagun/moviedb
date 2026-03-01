b0b69296603e0f21ae104931ce030252a080cd7f
juan <sxhxgun@gmail.com>
2026-03-01 15:03:29 -0800

    Modify Layer 1's file by adding DROP IF EXISTS statements, and then converting CREATE TABLE IF NOT EXISTS into just CREATE TABLE statements.

--- code/intermediate/1_interm_keys.sql
+++ code/intermediate/1_interm_keys.sql
@@ -1,13 +1,15 @@
 CREATE SCHEMA IF NOT EXISTS intermediate;
 
-CREATE TABLE IF NOT EXISTS intermediate.rated_movie_tconsts AS (
+DROP TABLE IF EXISTS intermediate.rated_movie_tconsts;
+CREATE TABLE intermediate.rated_movie_tconsts AS (
     SELECT tb.tconst AS tconst
     FROM staging.title_basics tb INNER JOIN staging.title_ratings tr
     ON tb.tconst = tr.tconst
     WHERE tb."titleType" = 'movie'
 );
 
-CREATE TABLE IF NOT EXISTS intermediate.rated_movie_nconsts AS (
+DROP TABLE IF EXISTS intermediate.rated_movie_nconsts;
+CREATE TABLE intermediate.rated_movie_nconsts AS (
     SELECT DISTINCT(nconst)
     FROM staging.title_principals
     WHERE tconst IN (SELECT tconst FROM intermediate.rated_movie_tconsts)
---
+++ code/intermediate/2_filtered_tables.sql
@@ -0,0 +1,56 @@
+DROP TABLE IF EXISTS intermediate.movie_basics;
+CREATE TABLE intermediate.movie_basics AS (
+    SELECT
+        "tconst" AS tconst,
+        "primaryTitle" AS primarytitle,
+        "originalTitle" AS originaltitle,
+        "isAdult" AS isadult,
+        "startYear" AS startyear,
+        "runtimeMinutes" AS runtimeminutes,
+        "genres" AS genres
+    FROM staging.title_basics
+    WHERE "tconst" IN (SELECT tconst FROM intermediate.rated_movie_tconsts)
+);
+
+DROP TABLE IF EXISTS intermediate.movie_crew;
+CREATE TABLE intermediate.movie_crew AS (
+    SELECT tconst, directors, writers
+    FROM staging.title_crew
+    WHERE tconst IN (SELECT tconst FROM intermediate.rated_movie_tconsts)
+);
+
+DROP TABLE IF EXISTS intermediate.movie_principals;
+CREATE TABLE intermediate.movie_principals AS (
+    SELECT
+        tconst,
+        ordering,
+        nconst,
+        category,
+        job,
+        characters
+    FROM staging.title_principals
+    WHERE tconst IN (SELECT tconst FROM intermediate.rated_movie_tconsts)
+);
+
+DROP TABLE IF EXISTS intermediate.movie_ratings;
+CREATE TABLE intermediate.movie_ratings AS (
+    SELECT
+        "tconst" AS tconst,
+        "averageRating" AS averagerating,
+        "numVotes" AS numVotes
+    FROM staging.title_ratings
+    WHERE tconst IN (SELECT tconst FROM intermediate.rated_movie_tconsts)
+);
+
+DROP TABLE IF EXISTS intermediate.people_info;
+CREATE TABLE intermediate.people_info AS (
+    SELECT
+        "nconst" AS nconst,
+        "primaryName" AS primaryname,
+        "birthYear" AS birthyear,
+        "deathYear" AS deathyear,
+        "primaryProfession" AS primaryprofession,
+        "knownForTitles" AS knownfortitles
+    FROM staging.name_basics
+    WHERE nconst IN (SELECT nconst FROM intermediate.rated_movie_nconsts)
+);
