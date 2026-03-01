db0e4b9ff69f48cb6ec36f784b46b7af95ffd848
juan <sxhxgun@gmail.com>
2026-03-01 14:45:13 -0800

    Create a file that drops all intermediate tables. Create another file that will be the first layer in the cleaning process: Create 2 tables, each with a single column, that only contain the tconsts of movies that have been rated at least once, and the nconsts corresponding to people that have completed a job on at least one rated film.

---
+++ code/intermediate/drop_interm.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS intermediate.movie_ratings;
+DROP TABLE IF EXISTS intermediate.movie_principals;
+DROP TABLE IF EXISTS intermediate.movie_crew;
+DROP TABLE IF EXISTS intermediate.movie_basics;
+DROP TABLE IF EXISTS intermediate.rated_movie_tconsts;
