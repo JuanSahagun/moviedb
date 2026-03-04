33804394136ec527a20be3005c0e0b66344c8316
juan <sxhxgun@gmail.com>
2026-03-04 11:17:48 -0800

    Create global variables for the maximum amount of movies to link, and the batch size. Create a string variable for the SELECT statement, which grabs the IMDB ids of the movies that it will attempt to link.

--- code/scripts/link_movies.py
+++ code/scripts/link_movies.py
@@ -6,10 +6,30 @@ from dotenv import load_dotenv
 import psycopg
 from psycopg.types.json import Jsonb
 
+# Max amount of movies to attempt to link.
+movie_limit =  1000
+# Batch size of each Linking & Writing iteration.
+batch_size = 100
+
+# * This query retrieves the IMDb movie ids that have a link status of 
+#   'pending' or 'error' (with an attempt limit of 3).
+# * Movies with higher vote counts are prioritized.
+select_tconst_sql = f"""
+SELECT tconst
+FROM link.movies l
+    INNER JOIN intermediate.rated_movies r
+    ON l.tconst = r.tconst
+WHERE l.link_status = 'pending'
+    OR (l.link_status = 'error' AND l.attempt_count < 3)
+ORDER BY r.numvotes DESC
+LIMIT {movie_limit}
+;
+"""
+
 # * This query will be used to update the status of a movie that was
 #   just attempted to be linked to the TMDB database.
 # * Replacement value order: (tmdb_id, link_status, result, last_error, tconst)
-update_sql = """
+update_status_sql = """
 UPDATE link.movies
 SET
     tmdb_id = %s,
