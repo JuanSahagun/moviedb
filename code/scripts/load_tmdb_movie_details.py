5840aff8e9686e788ece48f28460c91b21621e7c
juan <sxhxgun@gmail.com>
2026-03-13 18:25:01 -0700

    Create variables for the movie limit, batch size, and create a f-string for the SQL query that obtains the IMDB ID's it will try to find.

--- code/scripts/load_tmdb_movie_details.py
+++ code/scripts/load_tmdb_movie_details.py
@@ -9,3 +9,20 @@ from psycopg.types.json import Jsonb
 # Load API key from the enviornment variable
 load_dotenv()
 api_key = os.getenv('TMDB_API_KEY')
+
+# Max amount of movies to attempt to find.
+movie_limit =  10000
+# Batch size of each Linking & Writing iteration.
+batch_size = 100
+
+select_tmdb_sql = f"""
+SELECT tmdb_id
+FROM link.tmdb_movie_details t
+    INNER JOIN intermediate.movie_ratings r
+    ON t.tconst = r.tconst
+WHERE found_status = 'pending'
+    OR (found_status = 'error' AND attempt_count < 3)
+ORDER BY numvotes DESC
+LIMIT {movie_limit}
+;
+"""
