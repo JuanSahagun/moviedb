3e678560e9a6c0022bd174329ab84a8c2074e4cc
juan <sxhxgun@gmail.com>
2026-03-13 18:16:50 -0700

    Load the needed libraries, and the enviornment variable for the API key.

---
+++ code/scripts/load_tmdb_movie_details.py
@@ -0,0 +1,11 @@
+from requests_ratelimiter import LimiterSession
+
+import os
+from dotenv import load_dotenv
+
+import psycopg
+from psycopg.types.json import Jsonb
+
+# Load API key from the enviornment variable
+load_dotenv()
+api_key = os.getenv('TMDB_API_KEY')
